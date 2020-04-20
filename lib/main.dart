f40344b9242f0570f46bea4b35d6bda1fdfa2656
nabinrawat <nabinmehetar@gmail.com>
2020-04-20 11:05:25 +0545

    3# getting data back from second screen

--- lib/main.dart
+++ lib/main.dart
@@ -14,6 +14,22 @@ class Home extends StatefulWidget {
 
 class _HomeState extends State<Home> {
   var _nameFieldController = new TextEditingController();
+  Future _goToNextScreen(BuildContext context) async {
+    Map results = await Navigator.of(context).push(
+      new MaterialPageRoute(
+          builder: (BuildContext context){
+            return new NextScreen(name: _nameFieldController.text);
+          })
+    );
+    if (results != null && results.containsKey("info")) {
+      print(results['info'].toString());
+      _nameFieldController.text = results['info'].toString();
+    }
+    else {
+      print("Nothing!");
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return new Scaffold(
@@ -27,20 +43,14 @@ class _HomeState extends State<Home> {
           new ListTile(
             title: new TextField(
               controller: _nameFieldController,
-              decoration: new InputDecoration(
-                labelText: 'Enter Your Name'
-              ),
+              decoration: new InputDecoration(labelText: 'Enter Your Name'),
             ),
           ),
-
           new ListTile(
             title: new RaisedButton(
                 child: new Text("Send To Next Screen"),
-                onPressed: () {
-                  var router = new MaterialPageRoute(
-                      builder: (BuildContext context) => new NextScreen(name: _nameFieldController.text));
-                  Navigator.of(context).push(router);
-                }),
+                onPressed: () {_goToNextScreen(context);}
+            ),
           )
         ],
       ),
@@ -48,15 +58,17 @@ class _HomeState extends State<Home> {
   }
 }
 
-
 class NextScreen extends StatefulWidget {
   final String name;
+
   NextScreen({Key key, this.name}) : super(key: key);
+
   @override
   _NextScreenState createState() => _NextScreenState();
 }
 
 class _NextScreenState extends State<NextScreen> {
+  var _backTextFieldController = new TextEditingController();
 
   @override
   Widget build(BuildContext context) {
@@ -66,9 +78,32 @@ class _NextScreenState extends State<NextScreen> {
         title: new Text('Second Screen'),
         centerTitle: true,
       ),
-      body: new ListTile(
-        title: new Text('${widget.name}'),
+      body: new Container(
+        child: new Column(
+          children: <Widget>[
+            new ListTile(
+              title: new Text('${widget.name}'),
+            ),
+            new ListTile(
+              title: new TextField(
+                controller: _backTextFieldController,
+              ),
+            ),
+            new ListTile(
+              title: new FlatButton(
+                  onPressed: () {
+                    Navigator.pop(
+                        context, {'info': _backTextFieldController.text});
+                  },
+                  child: new Text('send Data Back')),
+            ),
+          ],
+        ),
       ),
+
+//      body: new ListTile(
+//        title: new Text('${widget.name}'),
+//      ),
     );
   }
 }
